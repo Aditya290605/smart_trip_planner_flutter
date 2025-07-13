1daddef3f9b9a0f41db593832efbe4847cf43880
aditya <magaraditya2911@gmail.com>
2025-07-13 23:31:31 +0530

    some ui changes added

--- smart_trip_planner/lib/features/trip_plan/presentation/pages/home_page.dart
+++ smart_trip_planner/lib/features/trip_plan/presentation/pages/home_page.dart
@@ -1,7 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smart_trip_planner/core/theme/app_color.dart';
 import 'package:smart_trip_planner/features/auth/presentation/bloc/auth_bloc_bloc.dart';
+import 'package:smart_trip_planner/features/trip_plan/presentation/widgets/primary_button.dart';
+import 'package:smart_trip_planner/features/trip_plan/presentation/widgets/text_box.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,16 +14,51 @@ class HomePage extends StatelessWidget {
       body: BlocBuilder<AuthBlocBloc, AuthBlocState>(
         builder: (context, state) {
           if (state is AuthSuccess) {
-            return Center(
-              child: GestureDetector(
-                onTap: () async {
-                  await FirebaseAuth.instance.signOut();
-                },
-                child: Text("hello ${state.user.name}"),
+            return SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Hey ${state.user.name} 👋',
+                          style: Theme.of(context).textTheme.headlineMedium!
+                              .copyWith(color: AppColors.primaryDark),
+                        ),
+                        CircleAvatar(
+                          child: Text(state.user.name.substring(0, 1)),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    Text(
+                      "What’s your vision",
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    Text(
+                      "for this trip?",
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    const SizedBox(height: 30),
+
+                    InputTextBox(),
+                    const SizedBox(height: 30),
+                    PrimaryButton(
+                      label: 'Create My Itinerary',
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ),
             );
           }
-          return Center(child: Text(""));
+          return Center(child: CircularProgressIndicator());
         },
       ),
     );
---
+++ smart_trip_planner/lib/features/trip_plan/presentation/widgets/text_box.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:smart_trip_planner/core/theme/app_color.dart';
+
+class InputTextBox extends StatelessWidget {
+  const InputTextBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14),
+        border: Border.all(color: AppColors.borderGreadient2),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          TextField(
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
+            maxLines: 4,
+            decoration: InputDecoration(
+              border: InputBorder.none, // Hide all borders
+              enabledBorder: InputBorder.none, // Hide when not focused
+              focusedBorder: InputBorder.none,
+              hint: Text(
+                "Start planing ...",
+                style: Theme.of(
+                  context,
+                ).textTheme.bodyLarge!.copyWith(color: AppColors.textLight),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(right: 12, bottom: 12),
+            child: Icon(Icons.mic, size: 30, color: AppColors.primaryDark),
+          ),
+        ],
+      ),
+    );
+  }
+}
